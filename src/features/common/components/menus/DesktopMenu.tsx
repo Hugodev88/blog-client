55a2202fe86c4d354f23e53db4b0a0bdcc27ae80
Hugo Almeida <88250740+Hugodev88@users.noreply.github.com>
2025-01-29 12:35:26 -0300

    feat: insert menu in desktopmenu

--- src/features/common/components/menus/DesktopMenu.tsx
+++ src/features/common/components/menus/DesktopMenu.tsx
@@ -1,6 +1,6 @@
 import { UserDto } from 'features/auth/dtos/user.dto'
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 const DesktopMenu = ({ user }: { user: UserDto | null }) => {
 	return (
@@ -9,6 +9,26 @@ const DesktopMenu = ({ user }: { user: UserDto | null }) => {
 			<NavLink to='/about' className={({ isActive }) => (isActive ? 'active' : '')}>About</NavLink>
 			{!user && (
 				<NavLink to='/auth' className={({ isActive }) => (isActive ? 'active' : '')}>Sign Up/In</NavLink>
+			)}
+			{user && (
+				<div className="dropdown">
+					<Link to="#">
+						<img src={user.avatar} alt={user.name} className='avatar-small' />
+						<span>{user.name}</span>
+					</Link>
+					<div className="dropdown-content">
+						<Link to='#'>Logout</Link>
+						{user.roles.find((role) => ['admin', 'author', 'reader'].includes(role)) && (
+							<>
+								<NavLink to="/create-post" className={({ isActive }) => (isActive ? 'active' : '')}>Create Post</NavLink>
+								<NavLink to="/create-category" className={({ isActive }) => (isActive ? 'active' : '')}>Create Category</NavLink>
+								<NavLink to="/create-tag" className={({ isActive }) => (isActive ? 'active' : '')}>Create Tag</NavLink>
+								<NavLink to="/approve" className={({ isActive }) => (isActive ? 'active' : '')}>Approve Posts</NavLink>
+								<NavLink to="/users/role" className={({ isActive }) => (isActive ? 'active' : '')}>Update Roles</NavLink>
+							</>
+						)}
+					</div>
+				</div>
 			)}
 		</div>
 	)
